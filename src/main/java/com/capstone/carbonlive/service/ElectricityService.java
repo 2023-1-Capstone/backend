802f4301259237259da73fd3de64b44ae2f1d3b8
ZZAMBAs <rksidksrksi@naver.com>
2023-04-19 00:08:53 +0900

    ElectricityRepository의 findAllById에 대한 테스트 생성

--- src/main/java/com/capstone/carbonlive/service/ElectricityService.java
+++ src/main/java/com/capstone/carbonlive/service/ElectricityService.java
@@ -1,7 +1,6 @@
 package com.capstone.carbonlive.service;
 
 import com.capstone.carbonlive.dto.UsageResult;
-import org.springframework.stereotype.Service;
 
 public interface ElectricityService {
     UsageResult getEachAll(Long id);

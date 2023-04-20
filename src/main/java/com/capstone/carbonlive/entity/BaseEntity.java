da3f050d7c700c820bac3ebc22d27ccbd389da37
ZZAMBAs <rksidksrksi@naver.com>
2023-04-20 22:25:53 +0900

    Refactor: 전기,가스,탄소,수도의 공통 부분을 BaseEntity 클래스로 나누고 해당 클래스를 상속한다.

---
+++ src/main/java/com/capstone/carbonlive/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.capstone.carbonlive.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@MappedSuperclass
+@Getter
+@ToString
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class BaseEntity {
+    @Column(nullable = false)
+    protected LocalDate recordedAt;
+
+    @Column(nullable = false)
+    protected int usages;
+}

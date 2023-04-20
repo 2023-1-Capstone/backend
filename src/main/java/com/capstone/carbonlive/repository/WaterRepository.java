e174cf9cb42f424db249ceb605ddc8d815dba7ea
ZZAMBAs <rksidksrksi@naver.com>
2023-04-20 22:44:09 +0900

    Feat: Water의 Repository, Service 계층을 추가한다.

---
+++ src/main/java/com/capstone/carbonlive/repository/WaterRepository.java
@@ -0,0 +1,7 @@
+package com.capstone.carbonlive.repository;
+
+import com.capstone.carbonlive.entity.Water;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WaterRepository extends JpaRepository<Water, Long> {
+}
---
+++ src/main/java/com/capstone/carbonlive/service/WaterServiceImpl.java
@@ -0,0 +1,23 @@
+package com.capstone.carbonlive.service;
+
+import com.capstone.carbonlive.dto.UsageResult;
+import com.capstone.carbonlive.entity.Water;
+import com.capstone.carbonlive.repository.WaterRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.capstone.carbonlive.service.common.GetUsageResult.getUsageResult;
+
+@Service
+@RequiredArgsConstructor
+public class WaterServiceImpl implements WaterService{
+    private final WaterRepository waterRepository;
+
+    @Override
+    public UsageResult getAll() {
+        List<Water> waterList = waterRepository.findAll();
+        return getUsageResult(waterList);
+    }
+}

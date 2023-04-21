0e1837c66481b5885bcccc86d211cd50df28adaf
ZZAMBAs <rksidksrksi@naver.com>
2023-04-21 16:01:04 +0900

    Test: Rest Docs 내 필드 설명을 추가한다.

--- src/test/java/com/capstone/carbonlive/controller/WaterControllerTest.java
+++ src/test/java/com/capstone/carbonlive/controller/WaterControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +17,12 @@ import java.util.stream.IntStream;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -48,12 +54,26 @@ class WaterControllerTest extends AbstractRestDocsTest {
         given(waterService.getAll())
                 .willReturn(usageResult);
 
-        mockMvc.perform(get(uri).contentType(MediaType.APPLICATION_JSON))
+        ResultActions resultActions = mockMvc.perform(get(uri).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.result[0].year").value(2018))
-                .andExpect(jsonPath("$.result[1].year").value(2019))
-                .andExpect(jsonPath("$.result[0].usages[0]").value(1))
-                .andExpect(jsonPath("$.result[1].usages[11]").value(16)); // 나중에 전체 검증 손 볼것.
+                .andExpect(jsonPath("$.result[1].year").value(2019));
+
+        for (int i = 0; i < 12; i++) {
+            resultActions.andExpect(jsonPath("$.result[0].usages[" + i + "]").value(i + 1));
+        }
+
+        resultActions.andExpect(jsonPath("$.result[1].usages[11]").value(16))
+                .andDo(print())
+                .andDo(document("{class-name}/{method-name}",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("result").description("결과 반환"),
+                                fieldWithPath("result[].year").description("년도"),
+                                fieldWithPath("result[].usages").description("해당 년도에 따른 월별 사용량 집합(1월-12월)")
+                        ))
+                );
 
         then(waterService).should(times(1)).getAll();
     }

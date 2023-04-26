aac7c56eb45b4ef310c998363e55ce903feac3a7
soohy <soohyun120@naver.com>
2023-04-26 13:13:44 +0900

    Feat: 계절별 전기 사용량 기능 추가

--- src/main/java/com/capstone/carbonlive/service/ElectricityService.java
+++ src/main/java/com/capstone/carbonlive/service/ElectricityService.java
@@ -1,8 +1,10 @@
 package com.capstone.carbonlive.service;
 
+import com.capstone.carbonlive.dto.SeasonResponse;
 import com.capstone.carbonlive.dto.UsageResponse;
 import com.capstone.carbonlive.dto.UsageResult;
 
 public interface ElectricityService {
     UsageResult<UsageResponse> getEachAll(Long id);
+    UsageResult<SeasonResponse> getSeasonData();
 }
--- src/main/java/com/capstone/carbonlive/service/ElectricityServiceImpl.java
+++ src/main/java/com/capstone/carbonlive/service/ElectricityServiceImpl.java
@@ -1,5 +1,6 @@
 package com.capstone.carbonlive.service;
 
+import com.capstone.carbonlive.dto.SeasonResponse;
 import com.capstone.carbonlive.dto.UsageResponse;
 import com.capstone.carbonlive.dto.UsageResult;
 import com.capstone.carbonlive.entity.Building;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 import static com.capstone.carbonlive.service.common.GetUsageResult.getBuildingUsageResult;
+import static com.capstone.carbonlive.service.common.GetUsageResult.getSeasonUsageResult;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +23,9 @@ public class ElectricityServiceImpl implements ElectricityService {
     private final ElectricityRepository electricityRepository;
     private final BuildingRepository buildingRepository;
 
+    /**
+     * 건물별 전기 사용량
+     */
     @Override
     public UsageResult<UsageResponse> getEachAll(Long id) {
         Building building = buildingRepository.findById(id).orElseThrow(EntityNotFoundException::new);
@@ -29,5 +34,15 @@ public class ElectricityServiceImpl implements ElectricityService {
                 Sort.by("recordedAt").ascending());
 
         return getBuildingUsageResult(electricityList);
+    }
+
+    /**
+     * 계절별 전기 사용량
+     */
+    @Override
+    public UsageResult<SeasonResponse> getSeasonData() {
+        List<Electricity> electricityList = electricityRepository.findAll(Sort.by("recordedAt").ascending());
+
+        return getSeasonUsageResult(electricityList);
     }
 }

b6a5104f1dd6d85c68f5a0fc0113eae2874a89fe
ZZAMBAs <rksidksrksi@naver.com>
2023-05-16 14:19:53 +0900

    Feat: 탄소 전체 배출량을 가져오는 서비스를 추가한다.

--- src/main/java/com/capstone/carbonlive/service/CarbonService.java
+++ src/main/java/com/capstone/carbonlive/service/CarbonService.java
@@ -3,8 +3,10 @@ package com.capstone.carbonlive.service;
 import com.capstone.carbonlive.dto.CarbonYearResponse;
 import com.capstone.carbonlive.dto.UsageResponse;
 import com.capstone.carbonlive.dto.UsageResult;
+import com.capstone.carbonlive.dto.UsageWithNameResponse;
 
 public interface CarbonService {
     UsageResult<CarbonYearResponse> getYearsUsages();
     UsageResult<UsageResponse> getBuildingUsages(Long buildingId);
+    UsageResult<UsageWithNameResponse> getAllUsages();
 }

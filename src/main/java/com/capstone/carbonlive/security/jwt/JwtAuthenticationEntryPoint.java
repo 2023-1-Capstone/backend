7aeb9a7e2571694b8d0432e7e54edb64d2c85b52
soohy <soohyun120@naver.com>
2023-05-30 02:34:42 +0900

    Feat: 예외 응답 객체 생성

--- src/main/java/com/capstone/carbonlive/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/capstone/carbonlive/security/jwt/JwtAuthenticationEntryPoint.java
@@ -1,21 +1,56 @@
 package com.capstone.carbonlive.security.jwt;
 
-import jakarta.servlet.ServletException;
+import com.capstone.carbonlive.errors.ErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-/**
- * 잘못된 토큰으로 접근한 경우
- */
+import static com.capstone.carbonlive.errors.ErrorCode.*;
+
+
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendRedirect("/exception/entry");
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        String exception = (String) request.getAttribute("exception");
+
+        //토큰 없는 경우
+        if (exception == null) {
+            setResponse(response, UNKNOWN_ERROR);
+        }
+        //잘못된 타입의 토큰인 경우
+        else if (exception.equals(WRONG_TYPE_TOKEN)) {
+            setResponse(response, WRONG_TYPE_TOKEN);
+        }
+        //만료된 토큰인 경우
+        else if (exception.equals(EXPIRED_TOKEN)) {
+            setResponse(response, EXPIRED_TOKEN);
+        }
+        //지원되지 않는 토큰인 경우
+        else if (exception.equals(UNSUPPORTED_TOKEN)) {
+            setResponse(response, UNSUPPORTED_TOKEN);
+        }
+        else {
+            setResponse(response, ACCESS_DENIED);
+        }
+    }
+
+    private void setResponse(HttpServletResponse response, ErrorCode code) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("status", code.getHttpStatus().value());
+        jsonObject.put("code", code.getHttpStatus());
+        jsonObject.put("message", code.getMessage());
+
+        response.getWriter().print(jsonObject);
     }
 }

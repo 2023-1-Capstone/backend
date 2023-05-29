4877a8f3182650aee41b14e9b10faed93ec8e153
soohy <soohyun120@naver.com>
2023-05-30 01:55:52 +0900

    Feat: VALIDATION 추가

--- src/main/java/com/capstone/carbonlive/service/common/redis/RedisKey.java
+++ src/main/java/com/capstone/carbonlive/service/common/redis/RedisKey.java
@@ -7,12 +7,13 @@ import lombok.Getter;
  * 이메일 인증      key(EMAILAUTH_email),         value(UUID)
  * Refresh Token  key(REFRESH_username),        value(refresh Token)
  * BlackList      key(BLACKLIST_access Token),  value(access Token)
+ * Validation     key(VALIDATION),              value(username)
  */
 
 @Getter
 @AllArgsConstructor
 public enum RedisKey {
-    EMAILAUTH("EMAILAUTH_"), REFRESH("REFRESH_"), BLACKLIST("BLACKLIST_");
+    EMAILAUTH("EMAILAUTH_"), REFRESH("REFRESH_"), BLACKLIST("BLACKLIST_"), VALIDATION("VALIDATION");
 
     private String key;
 }

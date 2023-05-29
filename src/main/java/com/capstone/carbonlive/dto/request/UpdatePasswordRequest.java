54524ea51f0b69b9edeb6fa6cae7e37647d5977b
soohy <soohyun120@naver.com>
2023-05-30 01:26:35 +0900

    Feat: UpdatePasswordRequest 생성자 추가

--- src/main/java/com/capstone/carbonlive/dto/request/UpdatePasswordRequest.java
+++ src/main/java/com/capstone/carbonlive/dto/request/UpdatePasswordRequest.java
@@ -1,8 +1,12 @@
 package com.capstone.carbonlive.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdatePasswordRequest {
     private String currentPw;
     private String newPw;

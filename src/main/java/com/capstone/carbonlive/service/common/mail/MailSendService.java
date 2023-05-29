2ce41afc3dd7b9dab20add0e6dcfc566377b6a2e
soohy <soohyun120@naver.com>
2023-05-30 01:45:53 +0900

    Feat: 이메일 제목 수정

--- src/main/java/com/capstone/carbonlive/service/common/mail/MailSendService.java
+++ src/main/java/com/capstone/carbonlive/service/common/mail/MailSendService.java
@@ -19,7 +19,7 @@ public class MailSendService {
     public void sendMail(String email, String authToken) {
         try {
             MailUtils sendMail = new MailUtils(javaMailSender);
-            sendMail.setSubject("인하대학교 이메일 인증");
+            sendMail.setSubject("[CARBONLIVE] 이메일 인증");
             sendMail.setText(new StringBuffer().append("<h1>[이메일 인증]</h1>")
                     .append("<p>아래 링크를 클릭하시면 이메일 인증이 완료됩니다.</p>")
                     .append("<a href='http://localhost:8080/api/user/joinConfirm?email=")
@@ -28,7 +28,7 @@ public class MailSendService {
                     .append(authToken)
                     .append("' target='_blenk'>이메일 인증 확인</a>")
                     .toString());
-            sendMail.setFrom("suhyun12090@gmail.com", "관리자");
+            sendMail.setFrom("suhyun12090@gmail.com", "Carbonlive");
             sendMail.setTo(email);
             sendMail.send();
 
@@ -45,12 +45,12 @@ public class MailSendService {
     public void sendMail_pw(String email, String tempPassword) {
         try {
             MailUtils sendMail = new MailUtils(javaMailSender);
-            sendMail.setSubject("임시 비밀번호 안내 이메일입니다.");
+            sendMail.setSubject("[CARBONLIVE] 임시 비밀번호 안내 이메일입니다.");
             sendMail.setText(new StringBuffer().append("<h1>[임시 비밀번호]</h1>")
-                    .append(email + "님의 임시 비밀번호는 " + tempPassword + "입니다."
+                    .append(email + "님의 임시 비밀번호는 " + tempPassword + " 입니다. "
                     + "로그인 후에 비밀번호를 반드시 변경 해주세요.")
                     .toString());
-            sendMail.setFrom("suhyun12090@gmail.com", "관리자");
+            sendMail.setFrom("suhyun12090@gmail.com", "Carbonlive");
             sendMail.setTo(email);
             sendMail.send();
 

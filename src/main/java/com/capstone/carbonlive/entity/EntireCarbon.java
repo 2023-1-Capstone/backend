ab14e632f01252dcca6581c7a6ad7809107185c6
soohy <soohyun120@naver.com>
2023-05-31 01:39:48 +0900

    Feat: EntireCarbon entity 추가

---
+++ src/main/java/com/capstone/carbonlive/entity/EntireCarbon.java
@@ -0,0 +1,27 @@
+package com.capstone.carbonlive.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EntireCarbon extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ENTIRE_CARBON_ID")
+    private Long id;
+
+    @Builder(toBuilder = true)
+    public EntireCarbon(LocalDate recordedAt, Integer usages) {
+        this.recordedAt = recordedAt;
+        this.usages = usages;
+    }
+}

57c55f7756da4d4573e301356ff9d95e85ca7a15
soohy <soohyun120@naver.com>
2023-05-31 01:40:06 +0900

    Feat: EntireCarbon repository 추가

---
+++ src/main/java/com/capstone/carbonlive/repository/EntireCarbonRepository.java
@@ -0,0 +1,7 @@
+package com.capstone.carbonlive.repository;
+
+import com.capstone.carbonlive.entity.EntireCarbon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntireCarbonRepository extends JpaRepository<EntireCarbon, Long> {
+}

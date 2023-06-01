da7d400dfb614125b8ef02caac6b8bae00e9c7a4
soohy <soohyun120@naver.com>
2023-06-02 00:12:42 +0900

    Feat: WRONG_REFRESH_TOKEN HttpStatus 수정

--- src/main/java/com/capstone/carbonlive/errors/ErrorCode.java
+++ src/main/java/com/capstone/carbonlive/errors/ErrorCode.java
@@ -17,7 +17,7 @@ public enum ErrorCode {
 
     INVALID_ACCESS_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않은 엑세스 토큰입니다."),
     NO_REFRESH_TOKEN(HttpStatus.NOT_FOUND, "존재하지 않는 리프레시 토큰입니다."),
-    WRONG_REFRESH_TOKEN(HttpStatus.NOT_FOUND, "잘못된 리프레시 토큰입니다."),
+    WRONG_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "잘못된 리프레시 토큰입니다."),
     NO_AUTH_TOKEN(HttpStatus.NOT_FOUND, "존재하지 않는 인증 토큰입니다."),
 
     DUPLICATED_REQUEST(HttpStatus.BAD_REQUEST, "중복된 요청입니다."),

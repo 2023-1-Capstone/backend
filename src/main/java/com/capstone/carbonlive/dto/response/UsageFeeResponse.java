96f639c8b2aba6df3343c26ae4323aaf9d41f090
soohy <soohyun120@naver.com>
2023-06-08 14:18:42 +0900

    Feat: prediction, fee_prediction 추가

--- src/main/java/com/capstone/carbonlive/dto/response/UsageFeeResponse.java
+++ src/main/java/com/capstone/carbonlive/dto/response/UsageFeeResponse.java
@@ -1,11 +1,17 @@
 package com.capstone.carbonlive.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UsageFeeResponse {
     private Integer usages;
     private Integer fee;
+    private Integer prediction;
+    private Integer fee_prediction;
 }

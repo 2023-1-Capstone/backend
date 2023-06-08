6e8cd94d6402c240058a7adbd502ba91b0880730
soohy <soohyun120@naver.com>
2023-06-08 14:25:25 +0900

    Feat: FeeBaseEntity 상속받도록 수정

--- src/main/java/com/capstone/carbonlive/entity/Water.java
+++ src/main/java/com/capstone/carbonlive/entity/Water.java
@@ -9,20 +9,19 @@ import java.time.LocalDate;
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Water extends PredictionBaseEntity {
+public class Water extends FeeBaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "WATER_ID")
     private Long id;
 
-    private Integer fee;
-
     @Builder(toBuilder = true)
-    public Water(LocalDate recordedAt, Integer usages, Integer prediction, Integer fee){
+    public Water(LocalDate recordedAt, Integer usages, Integer prediction, Integer fee, Integer fee_prediction){
         this.recordedAt = recordedAt;
         this.usages = usages;
         this.prediction = prediction;
         this.fee = fee;
+        this.fee_prediction = fee_prediction;
     }
 }
